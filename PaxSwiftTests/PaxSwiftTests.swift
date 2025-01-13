f0cb4cb15bfba5425f21702cbeffe8a53858caeb
Tom Nguyen <tom.nguyen@finix.com>
2025-01-13 01:42:07 -0800

    Initial Commit

---
+++ PaxSwiftTests/PaxSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaxSwiftTests.swift
+//  PaxSwiftTests
+//
+//  Created by Tom Nguyen on 1/13/25.
+//
+
+import Testing
+@testable import PaxSwift
+
+struct PaxSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
